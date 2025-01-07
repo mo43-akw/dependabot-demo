0fc06c967b2048717f0c9cf1ac582029576fa96c
Moritz Greiner <moritz.greiner@karcher.com>
2025-01-07 13:47:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const awsConfig = {
-  accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+  secret_scanning_sample_token: 'AKIAIOSFODNN7EXAMPLE',
   secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
   region: 'us-east-1'
 };
